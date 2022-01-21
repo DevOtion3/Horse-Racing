7e7236ba27cbee5e73c616c5ce7cb7e116629805
Tony Leva <58071254+DevOtion3@users.noreply.github.com>
2022-01-21 16:56:46 +0100

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,38 @@
-var raceLength = $(".container").css.width;
+// var horse1 = $("#horse1");
+// var horse2 = $("#horse2");
+// var horse3 = $("#horse3");
+// var horse4 = $("#horse4");
 var startRaceButton = $("#startRaceButton");
+var horseId = 0;
+var validateHorseNumber = $('#validateHorseNumber');
 
-startRaceButton.click(function(){
-    var horse1 = $("#horse1");
-    var horse2 = $("#horse2");
-    var horse3 = $("#horse3");
-    var horse1Speed = speed;
-    var horse2Speed = speed;
-    var horse3Speed = speed;
-    var speed =  Math.random() * 4;
-
-
-    if (horse1Speed === 1) {
-        horse1.css.marginLeft += "25px";
-    }   else if (horse1Speed === 2) {
-        horse1.css.marginLeft += "50px";
-    }else if (horse1Speed === 3) {
-        horse1.css.marginLeft += "75px";
-    }else if (horse1Speed === 4) {
-        horse1.css.marginLeft += "100px";
-    }
-    if (horse2Speed === 1) {
-        horse2.css.marginLeft += "25px";
-    }   else if (horse2Speed === 2) {
-        horse2.css.marginLeft += "50px";
-    }else if (horse2Speed === 3) {
-        horse2.css.marginLeft += "75px";
-    }else if (horse2Speed === 4) {
-        horse2.css.marginLeft += "100px";
-    }
-    if (horse3Speed === 1) {
-        horse3.css.marginLeft += "25px";
-    }   else if (horse3Speed === 2) {
-        horse3.css.marginLeft += "50px";
-    }else if (horse3Speed === 3) {
-        horse3.css.marginLeft += "75px";
-    }else if (horse3Speed === 4) {
-        horse3.css.marginLeft += "100px";
+validateHorseNumber.click(function(){
+console.log("clicked1")
+var selectHorseNumber = $("#selectHorseNumber");
+    
+    
+    for (var horseId = 0; horseNumber <= selectHorseNumber.val(); horseId++){
+        
+        var background = $('#background');
+        background.append('<div class = "horses" id = "horse' + horseId + '">' + '</div>');
+        var horseImage = $('#horse' + horseId);
+        horseImage.append('<img src="assets/img/cheval1.png"/>');
     }
 })
 
+    
+
+
+startRaceButton.click(function() {
+    console.log("clicked2");
+    
+    // var horse1Speed = Math.floor(Math.random() * 100);
+    // var horse2Speed = Math.floor(Math.random() * 100);
+    // var horse3Speed = Math.floor(Math.random() * 100);
+    // var horse4Speed = Math.floor(Math.random() * 100);
+
+    // horse1.css("marginLeft", horse1Speed + "%");
+    // horse2.css("marginLeft", horse2Speed + "%");
+    // horse3.css("marginLeft", horse3Speed + "%");
+    // horse4.css("marginLeft", horse4Speed + "%");
+})
